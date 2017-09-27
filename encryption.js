235986e667c890a64537d6402730419b7716e480
Peter Sarkis <peter@Peters-MacBook-Pro.local>
2017-09-27 10:41:35 -0500

    updated added levels

--- encryption.js
+++ encryption.js
@@ -1,5 +1,15 @@
 const {decrypt, encrypt, encryptA, encryptB} = require('./noLooking');
 
+// ======= Level 1 =========
+
+console.log(encryptB(''));
+
+//Desired ouput = imronwdems
+
+//use function decrypt only after lvl1 is solved!
+
+// ======== Level 2 ==========
+
 //Place your guess here!
 
 console.log(encrypt(''));
